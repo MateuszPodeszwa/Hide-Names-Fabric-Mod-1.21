e99d0e748021638f1dff251df5a160c63cd61332
Mateusz Podeszwa <podinatubie@gmail.com>
2025-03-14 20:03:29 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Hide-Names-Fabric-Mod-1.21
+ This is a simple Minecraft 1.21 Fabric Modification that removes player's tag name.
---
+++ src/main/java/net/mc121/hide_names_fabric/HideNamesFabricDataGenerator.java
@@ -0,0 +1,11 @@
+package net.mc121.hide_names_fabric;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HideNamesFabricDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
